c60108a3cf43111840a50e6073811788d717c314
Gunnarbjo <89361003+Gunnarbjo@users.noreply.github.com>
2021-12-09 19:13:19 +0000

    Update Krossgata.java

--- Vika12/Krossgata.java
+++ Vika12/Krossgata.java
@@ -10,31 +10,18 @@
  *****************************************************************************/
 
 public class Krossgata {
-
     public static void main(String[] args) {
         String[] strengir1 = { "afmæli", "bál", };
         StdOut.println(finnaOrd(strengir1));
 
         String[] strengir2 = { "bók", "aldur" };
         StdOut.println(finnaOrd(strengir2));
-
     }
-
-    /**
-     * Finnur hvort til er sameiginlegur bókstafur í orðunum s1 og s2 og skilar hlut af
-     * klasanum TvoOrd sem hefur orðin tvö og sæti sameiginlega bókstafsins. Ef engin
-     * sameiginlegur stafur finnst þá skilar aðferðin null hlut.
-     *
-     * @param s1 orð
-     * @param s2 orð
-     * @return hlutur af klasanum TvoOrd sem hefur orðin tvö og sæti sameiginlega bókstafsins
-     */
     public static TvoOrd samiStafur(String s1, String s2) {
         int n1 = s1.length();
         int n2 = s2.length();
         int pos1;
         int pos2;
-
         for (int i = 0; i < n1; i++) {
             if (!s1.equals(s2) && s2.contains(s1.substring(i, i + 1))) {
                 pos1 = i;
@@ -48,16 +35,6 @@ public class Krossgata {
         }
         return null;
     }
-
-    /**
-     * Finnur tvö mismunnandi í fylki af orðum  sem hafa sameiginlegan staf og skilar hlut af
-     * klasanum TvoOrd sem inniheldur orðin tvö og sætin með sameiginlega stafnum
-     *
-     * @param ordK fylki af orðum
-     * @return hlutur af klasanum TvoOrd sem hefur orðin tvö og
-     * sæti sameiginlega bókstafsins. Ef engin sameiginlegur stafur finnst þá skilar
-     * aðferðin null hlut
-     */
     public static TvoOrd finnaOrd(String[] ordK) {
         for (int i = 0; i < ordK.length; i++) {
             TvoOrd k = finnaOrd(ordK, ordK[i]);
@@ -68,16 +45,6 @@ public class Krossgata {
         return null;
     }
 
-    /**
-     * Finnur fyrsta orð í  fylkinu ordK sem hafa sameiginlegan bókstaf með s
-     * Skilar orðunum tveimur með sætum sameiginlega bókstafsins.Ef engin sameiginlegur stafur
-     * finnst þá skilar aðferðin null hlut
-     *
-     * @param ordK fylki af orðum
-     * @param s    orð
-     * @return hlutur af klasanum TvoOrd sem hefur orðin tvö og sæti sameiginlega bókstafsins.
-     * skilar null ef enginn sameiginlegur
-     */
     public static TvoOrd finnaOrd(String[] ordK, String s) {
         for (int i = 0; i < ordK.length; i++) {
             TvoOrd t = samiStafur(ordK[i], s);
